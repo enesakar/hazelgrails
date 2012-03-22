eecd691ae516cb4a9130081446f89d71651f2017
Enes Akar <enesakar@gmail.com>
2012-03-22 18:17:59 +0200

    plugin main directory

---
+++ grails-app/services/hazelgrails/HazelService.groovy
@@ -0,0 +1,79 @@
+package hazelgrails
+
+import com.hazelcast.core.HazelcastInstance
+import com.hazelcast.core.Hazelcast
+import com.hazelcast.core.ITopic
+import java.util.concurrent.locks.Lock
+import com.hazelcast.core.IdGenerator
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Callable
+import java.util.concurrent.Future
+import com.hazelcast.core.MultiTask
+import com.hazelcast.core.Member
+import com.hazelcast.core.DistributedTask
+import java.util.concurrent.FutureTask
+import com.hazelcast.config.Config
+import com.hazelcast.config.FileSystemXmlConfig
+import org.codehaus.groovy.grails.commons.ConfigurationHolder
+
+class HazelService {
+    def grailsApplication
+    static transactional = false
+    HazelcastInstance instance
+
+    private HazelService() {
+        this.instance = Hazelcast.newHazelcastInstance(null)
+    }
+
+    Map map(String mapName) {
+        instance.getMap(mapName)
+    }
+
+    Queue queue(String queueName) {
+        instance.getQueue(queueName)
+    }
+
+    ITopic topic(String topicName) {
+        instance.getTopic(topicName)
+    }
+
+    Set hashset(String setName) {
+        instance.getSet(setName)
+    }
+
+    List list(String listName) {
+        instance.getList(listName)
+    }
+
+    Lock lock(Object lock) {
+        instance.getLock(lock)
+    }
+
+    long generateId(String name) {
+        IdGenerator idGenerator = Hazelcast.getIdGenerator(name);
+        idGenerator.newId();
+    }
+
+    Collection executeOnAllMembers(Callable callable) {
+        Collection<Member> members = instance.getCluster().getMembers()
+        MultiTask<String> multitask = new MultiTask<String>(callable, members);
+        ExecutorService executorService = Hazelcast.getExecutorService();
+        executorService.execute(multitask);
+        return multitask.get();
+    }
+
+    Object executeOnSomewhere(Callable callable) {
+        ExecutorService executorService = Hazelcast.getExecutorService();
+        Future task = executorService.submit(callable);
+        return task.get();
+    }
+
+    Object executeOnMemberOwningTheKey(Callable callable, Object key) {
+        FutureTask task = new DistributedTask(callable, key);
+        ExecutorService executorService = Hazelcast.getExecutorService();
+        executorService.execute(task);
+        return task.get();
+    }
+
+
+}
